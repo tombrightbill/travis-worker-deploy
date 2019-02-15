5512225270a3a70b963547d35306825cc4ca391a
Tom Brightbill <tbrightbill@cloudflare.com>
2019-02-15 09:54:49 -0800

    updated package

--- deploy.js
+++ deploy.js
@@ -5,10 +5,6 @@ const cloudflare = require('cloudflare-workers-toolkit')
 const fs = require('fs')
 const worker = fs.readFileSync('./worker.js', "utf8")
 
-cloudflare.setZoneId('32fc4108517d20341422b768ab90be38')
-
-accountId = '390226513f11a1cb8815f2009cbc5576'
-
 cloudflare.workers.deploy({
 	script: worker
 }).then((res) => {
