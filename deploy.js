5c099817a4746516f32aeb9fba28bc07480aae58
Tom Brightbill <tbrightbill@cloudflare.com>
2019-03-05 14:18:36 -0800

    deploy and travis

--- deploy.js
+++ deploy.js
@@ -1,16 +1,23 @@
-
-
-const cloudflare = require('cloudflare-workers-toolkit')
-
 const fs = require('fs')
+const fetch = require('node-fetch')
 const worker = fs.readFileSync('./worker.js', "utf8")
 
-cloudflare.workers.deploy({
-	script: worker
-}).then((res) => {
-	console.log(JSON.stringify(res, undefined, 2))
-}).catch((e) => {
-	console.log(e.message)
-})
 
+async function deployToCF()
+{
+  const headers = {
+  	'X-Auth-Key': process.env.CLOUDFLARE_AUTH_EMAIL,
+  	'X-Auth-Email': process.env.CLOUDFLARE_AUTH_KEY,
+  	'Content-Type': 'application/javascript'
+  }
 
+  const url = `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/workers/scripts/deployed-from-travis`
+  // NOTE: this is the Enterprise URL, if you are using a self service account the endpoint is:
+  // client/v4/zones/:zone_id/workers/script
+  // An Enterprise account can have multiple scripts, each with a unique name (in this case 'deployed-from-travis')
+  let data = await fetch(url, {method: 'PUT', headers: headers, body: worker})
+  let main = await data.json();
+  console.log(main);
+}
+
+deployToCF();
